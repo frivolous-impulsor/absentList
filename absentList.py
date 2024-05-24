b5a3f5d27ef76d9d5c69e0bad23f27adcfdd494f
admin_li2177 <admin_li2177@ads.mcmaster.ca>
2024-05-24 11:19:42 -0400

    translate csv to xlsx and organized hierarchy of minor files

--- absentList.py
+++ absentList.py
@@ -1,33 +1,67 @@
 import os
+import sys
 from os import listdir
 from os.path import isfile, join
 import openpyxl
 import re
-from openpyxl import Workbook
+import csv
 
 possibleColTitle = ["id", "first name", "name", "student id"]
 
-searchField = 'ID'
+searchField = ['id', 'student id']
 myPath = os.getcwd()
-thisFile = __file__
-minorfiles = [f for f in listdir(myPath) if (isfile(join(myPath, f)) and bool(re.search('.xlsx', f)) and f != "master.xlsx")]
+logDir = 'checkInLogs'
+minorDir = join(myPath, logDir)
 
-def findTitleRow(sheet):
+def isExactOneArg() -> bool:
+    return len(sys.argv) == 2
+
+def getMasterFile() -> str:
+    if isExactOneArg():
+        return sys.argv[1]
+    raise ValueError("Ensure Exactly One Master Sheet After the Program!")
+
+def getMinorFiles() -> list[str]:
+    return [join(minorDir, f) for f in listdir(minorDir) if (isfile(join(minorDir, f)) and bool(re.search('.xlsx', f)) and f != getMasterFile())]
+
+def translateCSV2XLSX(csvAddress: str):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    
+    with open(csvAddress) as csvFile:
+        csvData = csv.reader(csvFile, delimiter=',')
+    for row in csvData:
+        sheet.append(row)
+    xlsxAddress = csvAddress.replace(".csv", ".xlsx")
+    workbook.save(xlsxAddress)
+
+def ensureXLSX(fileName):
+    excelExt = '.xlsx'
+    csvExt = '.csv'
+    if excelExt in fileName and not (csvExt in fileName):
+        return 0
+    if csvExt in fileName and not (excelExt in fileName):
+        translateCSV2XLSX(fileName)
+        return 1
+    raise TypeError("checkInLogs consists files outside xlsx or csv! remove them")
+    
+    
+def findTitleRow(sheet) -> int:
     for r in range(1, 5):
         for c in range(1, sheet.max_column+1):
-            if (sheet.cell(r, c).value != None) and (sheet.cell(r, c).value.lower() in possibleColTitle):
+            if (sheet.cell(r, c).value != None) and type(sheet.cell(r,c).value) == type('str') and (sheet.cell(r, c).value.lower() in possibleColTitle):
                 return r
     raise ValueError("No Proper Title Row Detected, Ensure The Tile Row Contains One Of The Possible Col Title")
 
-def findColByName(sheet, colName: str):
-    IDcol = 0
-    colName = colName.lower()
+def findColByTitles(sheet, titles: list[str]):
     titleRow = findTitleRow(sheet)
     for c in range(1, sheet.max_column+1):
-        if sheet.cell(titleRow, c).value.lower() == colName:
+        if (type(sheet.cell(titleRow, c).value) == type('str')) and (sheet.cell(titleRow, c).value.lower() in titles):
             return c
     raise ValueError("Make Sure \"ID\" or \"student id\" Is One Of the Col Title, or Update findColByName Function")
 
+def findDataStartingRow(sheet):
+    "todo"
 
 
 def setIDsDict(path: str, idDict: dict, status: bool):
@@ -35,10 +69,10 @@ def setIDsDict(path: str, idDict: dict, status: bool):
     sheet = book.active
     maxRow = sheet.max_row
     titleRow = findTitleRow(sheet)
-    Col = findColByName(sheet, searchField)
+    titleCol = findColByTitles(sheet, searchField)
 
     for r in range(titleRow+1, maxRow+1):
-        id = sheet.cell(r, Col).value
+        id = sheet.cell(r, titleCol).value
         idDict[id] = status
 
 
@@ -54,5 +88,5 @@ def diffIDs(masterPath: str, minorPaths: list[str]):
             absentIDs.append(key)
     return absentIDs
 
-resultIDs = diffIDs("master.xlsx", minorfiles)
+resultIDs = diffIDs("master.xlsx", getMinorFiles())
 print(resultIDs)
